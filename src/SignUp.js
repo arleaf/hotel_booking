876c5ab958266664e874eb6d02b07f8b462f5412
Arleth <62979596+arleaf@users.noreply.github.com>
2024-07-28 20:34:53 -0700

    Added validation rules to registration

--- src/SignUp.js
+++ src/SignUp.js
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 
 //Sign Up page
 const SignUp = () => {
+
+    const [errors, setErrors] = useState({});
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
@@ -24,8 +26,38 @@ const SignUp = () => {
             username: username,
             password: password,
         };
-        saveUser(user);
+
+        //validate errors
+        const newErrors = validateForm(user);
+        setErrors(newErrors);
+
+        if (Object.keys(newErrors).length === 0) {
+            // Form submission logic here
+            console.log('Form submitted successfully!');
+            saveUser(user);
+        } else {
+            console.log(`Form submission failed
+             due to validation errors.`);
+        }
+
+
     }
+    //This method will be used to validate input signup data
+    const validateForm = (data) => {
+        const errors = {};
+        //validate username is not null
+        if (!data.username.trim()) {
+            errors.username = 'Username is required';
+        }
+        //Validate password meet rquirements
+        if (!data.password) {
+            errors.password = 'Password is required';
+        } else if (data.password.length < 8) {
+            errors.password = `Password must be at 
+            least 8 characters long`;
+        }
+        return errors;
+    };
 
     const saveUser = async (user) => {
         //redirect to available rooms once created
@@ -39,11 +71,21 @@ const SignUp = () => {
             <div className="form-group">
                 <label for="Email Address">Username</label>
                 <input type="username" className="form-control" id="username" aria-describedby="emailHelp" value={username} onChange={addUsername} />
+                {errors.username &&
+                    <span className="error-message">
+                        {errors.username}
+                    </span>
+                }
 
             </div>
             <div className="form-group">
                 <label for="exampleInputPassword1">Password</label>
                 <input type="password" className="form-control" id="password" value={password} onChange={addPassword} />
+                {errors.password &&
+                    <span className="error-message">
+                        {errors.password}
+                    </span>
+                }
             </div>
             <button type="submit" className="btn btn-primary">Submit</button>
         </form>
